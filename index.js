09fdaa6968318335b8ce099ed1275af8ad9c7c43
Mubor <muhatasov22@ukr.net>
2022-02-07 21:56:28 +0200

    spread added

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 let sum = arr => arr.reduce( (summa, elem) => summa + elem);
 
-let getValuesFromArray = objectArr => {
-  let res = [];
-  
-  for (let i = 0; i < objectArr.length; i++) {
-    res.push(parseInt(objectArr[i].innerHTML));
-  }
-  
-  return res;
-}
-
 let markObj = document.querySelectorAll('.widget-table__row-description > .ng-star-inserted:last-child')
-let marks = getValuesFromArray(markObj);
-
-let count = sum(marks);
+let count = sum([...markObj].map(el => el.innerHTML));
 
 console.log(count);
